af8e1a9e7c0c4b01cdf28c9f9d2f9382ee2202d3
shamshadAhmad-byte <shamshad1924138@gmail.com>
2026-08-07 09:13:07 +0530

    first change

--- src/db/mongo_client.py
+++ src/db/mongo_client.py
@@ -4,7 +4,7 @@ MongoDB client initialization.
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = "mongodb+srv://shamshad1924138:UXSFDMWzsWPffRZR@cluster0.uzfcu.mongodb.net/"
 DB_NAME = "adaptive_rag"
 
 client = AsyncIOMotorClient(MONGO_URL)
